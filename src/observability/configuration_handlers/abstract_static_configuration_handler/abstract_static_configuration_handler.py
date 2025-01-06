a27a71ed751b5aa858ca66b7446d9cff5e02fb15
user <user@DESKTOP-PGPMA4E>
2025-01-06 23:15:36 +0200

    feat - added docostrings to configuration handlers functions and classes

--- src/observability/configuration_handlers/abstract_static_configuration_handler/abstract_static_configuration_handler.py
+++ src/observability/configuration_handlers/abstract_static_configuration_handler/abstract_static_configuration_handler.py
@@ -3,8 +3,27 @@ from src.observability.configuration_handlers.abstract_configuration_handler imp
 
 
 class AbstractStaticConfigurationHandler(AbstractConfigurationHandler):
-    @staticmethod
-    def get_configuration_handler() -> ConfigurationHandlerType:
-        if AbstractStaticConfigurationHandler.config_handler is None:
-            AbstractStaticConfigurationHandler.update_config_handler()
-        return AbstractStaticConfigurationHandler.config_handler
+    """
+    AbstractStaticConfigurationHandler is a subclass of AbstractConfigurationHandler that manages
+    static configuration handling. It is responsible for retrieving and updating the configuration handler
+    for a given class.
+
+    Inherits from:
+        AbstractConfigurationHandler: The base class for all configuration handlers.
+    """
+
+    @classmethod
+    def get_configuration_handler(cls) -> ConfigurationHandlerType:
+        """
+        Retrieves the configuration handler for the class. If it has not been set,
+        it invokes the update_config_handler method to set it.
+
+        Args:
+            cls: The class invoking this method. It should be a subclass of AbstractStaticConfigurationHandler.
+
+        Returns:
+            ConfigurationHandlerType: The configuration handler instance for the class.
+        """
+        if cls.config_handler is None:
+            cls.update_config_handler()
+        return cls.config_handler
