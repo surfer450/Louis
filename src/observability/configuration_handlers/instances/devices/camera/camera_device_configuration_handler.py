1b38a9efe38df2aaf3678b5462ef5c61c2f97ef2
user <user@DESKTOP-PGPMA4E>
2025-01-07 23:44:41 +0200

    feat - fix the configuration handler to be threads safe and singleton

---
+++ src/observability/configuration_handlers/instances/devices/camera/camera_device_configuration_handler.py
@@ -0,0 +1,13 @@
+from src.observability.configuration_handlers.abstract_configuration_handler import AbstractConfigurationHandler
+
+
+class CameraDeviceConfigurationHandler(AbstractConfigurationHandler):
+    """
+    A dynamic configuration handler for camera devices.
+
+    This class provides a mechanism to manage and update configuration
+    settings for camera devices dynamically.
+    """
+    def get_config_path(self) -> str:
+        return "configurations/devices/camera/camera_device_config.json"
+
